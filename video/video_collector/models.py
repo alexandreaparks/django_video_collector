9bf592045dbfdda37c60c790b8c3a8b00690e0fe
alexandrea parks <alparks@idk.com>
2024-04-20 21:11:42 -0500

    modified model to include video_id

--- video/video_collector/models.py
+++ video/video_collector/models.py
@@ -5,6 +5,7 @@ class Video(models.Model):
     name = models.CharField(max_length=200)
     url = models.CharField(max_length=400)
     notes = models.TextField(blank=True, null=True)
+    video_id = models.CharField(max_length=40, unique=True)  # YouTube video url ID - no duplicates allowed
 
     def __str__(self):  # for development - user will not see this!
-        return f'ID: {self.pk}, Name: {self.name}, URL: {self.url}, Notes: {self.notes[:200]}'
+        return f'ID: {self.pk}, Name: {self.name}, URL: {self.url}, Video ID: {self.video_id}, Notes: {self.notes[:200]}'
